0d032c9dd0ac39efce8982807fab17c747145d0b
nakko <langonebros2@gmail.com>
2024-03-15 21:25:15 +0100

    created movie cards in js, waiting for api completion to proceed

--- slim-master/front-end/script.js
+++ slim-master/front-end/script.js
@@ -1,10 +1,19 @@
-axios.get("http://localhost/getMovies").then(response => {
+//remember to create vendor:
+//docker-compose exec webserver-dev composer update
+
+
+const instance = axios.create({
+    baseURL: 'http://localhost',
+    timeout: 1000,
+  });
+
+instance.get("/getMovies").then(response => {
     response.data.forEach(movie => {
         console.log(movie);
         let father = document.getElementById("list");
-        let div = document.createElement("div");
+        let div1 = document.createElement("div");
 
-        div.classList.add(
+        div1.classList.add(
             "group", 
             "text-white", 
             "transition-all", 
@@ -19,27 +28,55 @@ axios.get("http://localhost/getMovies").then(response => {
 
         image = new Image;
         image.src = "https://picsum.photos/300/200"; //remember to change with move.poster
-        //div.appendChild(image);
+        image.classList.add("w-full", "h-auto");
+        div1.appendChild(image);
 
-        //add 2 <p> for title and for review average and one below for description (follow example)
-        
-        father.appendChild(div);
+        div2 = document.createElement("div");
+        div2.classList.add("bg-gray-800", "opacity-0", "group-hover:opacity-100", "transition-all", "ease-in-out", "delay-150", "duration-500", "min-w-min", "p-3")
+;
+        div3 = document.createElement("div");
+        div3.classList.add("flex", "justify-end");
+
+        title = document.createElement("p");
+        title.classList.add("text-xl", "font-bold", "truncate");
+        title.innerText = movie.title;
+
+        avg = document.createElement("p");
+        avg.classList.add("ml-auto");
+        //api is still not complete
+        avg.innerText = /*getReviewAvg(movie.id) +*/ "Avg/5";
+
+        desc = document.createElement("p");
+        desc.classList.add("text-white");
+        //to implement when api is done
+        desc.innerText = "no description"
+
+        div3.appendChild(title);
+        div3.appendChild(avg)
+        div2.appendChild(div3);
+        div2.appendChild(desc)
+        div1.appendChild(div2);
+        father.appendChild(div1);
     })
 }).catch(error => {
     console.log(error);
 })
 
-axios.get("http://localhost/getActorsByMovie", {
-    params: {
-        id: '2'
-    }
-}).then(response => {
-    console.log(response);
-}).catch(err => {
-    console.log(err);
-})
+function getReviewAvg(movieId){
+    instance.get("/getReviewAverageByMovie", {
+        params:{
+            id: movieId
+        }
+    }).then(response => {
+        console.log(response)
+        return response;
+    })
+}
 
-
+function getDesc(movieId){
+    //gotta wait for the api :)
+    //instance.get("")
+}
 
 
 // const options = {
