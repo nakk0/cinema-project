43e61f70a0f0d79b86ee2de84febb97cbff75ec9
nakko <langonebros2@gmail.com>
2024-03-16 18:45:39 +0100

    made colNum() and made animation faster

--- slim-master/front-end/script.js
+++ slim-master/front-end/script.js
@@ -1,67 +1,88 @@
 //remember to create vendor:
 //docker-compose exec webserver-dev composer update
 
-
 const instance = axios.create({
     baseURL: 'http://localhost',
     timeout: 1000,
   });
 
-instance.get("/getMovies").then(response => {
-    response.data.forEach(movie => {
-        console.log(movie);
-        let father = document.getElementById("list");
-        let div1 = document.createElement("div");
+window.addEventListener("load", () => {
+    console.log("w")
+    displayMovies()
+});
 
-        div1.classList.add(
-            "group", 
-            "text-white", 
-            "transition-all", 
-            "ease-in-out", 
-            "delay-150",
-            "duration-500", 
-            "transform",
-            "hover:translate-y-1",  
-            "hover:scale-150",
-            "hover:bg-gray-800",
-            "hover:z-10")
-
-        image = new Image;
-        image.src = "https://picsum.photos/300/200"; //remember to change with move.poster
-        image.classList.add("w-full", "h-auto");
-        div1.appendChild(image);
-
-        div2 = document.createElement("div");
-        div2.classList.add("bg-gray-800", "opacity-0", "group-hover:opacity-100", "transition-all", "ease-in-out", "delay-150", "duration-500", "min-w-min", "p-3")
-;
-        div3 = document.createElement("div");
-        div3.classList.add("flex", "justify-end");
-
-        title = document.createElement("p");
-        title.classList.add("text-xl", "font-bold", "truncate");
-        title.innerText = movie.title;
-
-        avg = document.createElement("p");
-        avg.classList.add("ml-auto");
-        //api is still not complete
-        avg.innerText = /*getReviewAvg(movie.id) +*/ "Avg/5";
-
-        desc = document.createElement("p");
-        desc.classList.add("text-white");
-        //to implement when api is done
-        desc.innerText = "no description"
-
-        div3.appendChild(title);
-        div3.appendChild(avg)
-        div2.appendChild(div3);
-        div2.appendChild(desc)
-        div1.appendChild(div2);
-        father.appendChild(div1);
-    })
-}).catch(error => {
-    console.log(error);
+window.addEventListener("resize", () => {
+    //when the number of columns changes (colNum()), run displayMovies()
+    //implement the different animations based on position and col number in movies array.
 })
 
+function displayMovies(){
+    instance.get("/getMovies").then(response => {
+        let father = document.getElementById("list");
+        father.innerHTML = '';
+        movies = response.data;
+        console.log(movies);
+        movies.forEach((movie, i) => {
+            
+            let div1 = document.createElement("div");
+
+            div1.classList.add(
+                "group", 
+                "text-white", 
+                "sm:transition-all", 
+                "sm:ease-in-out", 
+                "sm:hover:delay-200",
+                "sm:duration-200", 
+                "sm:transform",
+                "sm:hover:scale-150",
+                "sm:hover:bg-gray-800",
+                "sm:hover:z-10")
+    
+            image = new Image;
+            image.src = "https://picsum.photos/300/200"; //remember to change with move.poster
+            image.classList.add("w-full", "h-auto");
+            div1.appendChild(image);
+    
+            div2 = document.createElement("div");
+            div2.classList.add(
+                "bg-gray-800", 
+                "sm:opacity-0", 
+                "sm:group-hover:opacity-100", 
+                "sm:transition-all", 
+                "sm:ease-in-out", 
+                "sm:delay-200", 
+                "min-w-min", 
+                "p-3"
+            )
+    ;
+            div3 = document.createElement("div");
+            div3.classList.add("flex", "justify-end");
+    
+            title = document.createElement("p");
+            title.classList.add("text-xl", "font-bold", "truncate", "max-w-56");
+            title.innerText = movie.title;
+    
+            avg = document.createElement("p");
+            avg.classList.add("ml-auto");
+            //api is still not complete
+            avg.innerText = /*getReviewAvg(movie.id) +*/ "*/5";
+    
+            desc = document.createElement("p");
+            desc.classList.add("text-white");
+            //to implement when api is done
+            desc.innerText = "no description"
+    
+            div3.appendChild(title);
+            div3.appendChild(avg)
+            div2.appendChild(div3);
+            div2.appendChild(desc)
+            div1.appendChild(div2);
+            father.appendChild(div1);
+        })
+    }).catch(error => {
+        console.log(error);
+    })
+}
 function getReviewAvg(movieId){
     instance.get("/getReviewAverageByMovie", {
         params:{
@@ -78,20 +99,18 @@ function getDesc(movieId){
     //instance.get("")
 }
 
-
-// const options = {
-//   method: 'GET',
-//   url: 'http://localhost/getActorsByFilm',
-//   headers: {
-//     cookie: 'PHPSESSID=d26e6ebb79100072a5c9fc8e5794b6ad',
-//     'Content-Type': 'application/json',
-//     'User-Agent': 'insomnia/8.6.1'
-//   },
-//   data: {id: '2'}
-// };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
+function colNum(){
+    
+    width = document.documentElement.clientWidth;
+    if(width >= 1536){
+        return 5;
+    }else if(width >= 1280){
+        return 4;
+    }else if(width >= 1024){
+        return 3
+    }else if(width >= 640){
+        return 2;
+    }else {
+        return 1;
+    };
+}
